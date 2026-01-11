264a9a30b4b996b4186d344d4c74228bd78af621
alx <alx@kali>
2026-01-11 17:22:36 -0500

    prints para debug

--- app/database.py
+++ app/database.py
@@ -1,15 +1,25 @@
-# app/database.py
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./quakepredict.db")
+# Leemos la variable de entorno DATABASE_URL
+url_entorno = os.getenv("DATABASE_URL")
+
+print("----------------------DEBUG----------------------------")
+if url_entorno:
+    print(f"VARIABLE ENCONTRADA: {url_entorno[:15]}...") 
+    print("BASE DE DATOS: PostgreSQL")
+else:
+    print("VARIABLE 'DATABASE_URL' NO DETECTADA O VACÍA")
+    print("BASE DE DATOS: SQLite")
+print("--------------------------------------------------")
+
+SQLALCHEMY_DATABASE_URL = url_entorno if url_entorno else "sqlite:///./quakepredict.db"
 
 if SQLALCHEMY_DATABASE_URL and SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
     SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 if "sqlite" in SQLALCHEMY_DATABASE_URL:
-
     engine = create_engine(
         SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
     )
@@ -17,10 +27,8 @@ else:
     engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
-# dependencia para fastapi
 def get_db():
     db = SessionLocal()
     try:
